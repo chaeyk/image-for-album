d547ec67d7bd61ce01eac99aa47d2fb4bf046ce7
chaeyk <chaeyk@gmail.com>
2023-04-23 02:05:43 +0900

    목표 사이즈에 따라 폰트 크기 조정.

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ from pathlib import Path
 import traceback
 import re
 
-def loadFont(fontfiles):
+# ratio1:ratio2 비율을 맞추려면 value1과 같은 비율의 value2는 얼마인가?
+def ratioTarget(ratio1, ratio2, value1):
+  return value1 * ratio2 / ratio1
+
+def loadFont(fontfiles, image_width, image_height):
+  # 이미지 크기가 1000 x 1334 일때 폰트 크기 40
+  size_by_width = ratioTarget(1000, 60, image_width)
+  size_by_height = ratioTarget(1334, 60, image_height)
+  size = round(min(size_by_width, size_by_height))
   for fontfile in fontfiles:
     try:
-      return ImageFont.truetype(fontfile, 35)
+      return ImageFont.truetype(fontfile, size)
     except:
       pass
 
@@ -52,11 +60,11 @@ def processImage(filepath, filename, outpath, target_width, target_height):
           exif[Base.Orientation.value] = 0
 
       # 가로 세로 비율이 안맞으면 잘라서 맞춘다
-      profitHeight = image.width * target_height / target_width
+      profitHeight = ratioTarget(target_width, target_height, image.width)
       if image.height > profitHeight:
         image = image.crop((0, (image.height - profitHeight) / 2, image.width, (image.height + profitHeight) / 2))
 
-      profitWidth = image.height * target_width / target_height
+      profitWidth = ratioTarget(target_height, target_width, image.height)
       if image.width > profitWidth:
         image = image.crop(((image.width - profitWidth) / 2, 0, (image.width + profitWidth) / 2, image.height))
 
@@ -68,7 +76,10 @@ def processImage(filepath, filename, outpath, target_width, target_height):
       dt = getImageDateTime(exif, filename)
       if dt:
         draw = ImageDraw.Draw(image)
-        draw.text((image.width - 70, image.height - 50), dt, font=font, anchor='rs', fill='orange', stroke_fill='black', stroke_width=2)
+        # 텍스트 출력할 위치
+        pos_x = ratioTarget(1000, 70, image.width)
+        pos_y = ratioTarget(1334, 50, image.height)
+        draw.text((image.width - pos_x, image.height - pos_y), dt, font=font, anchor='rs', fill='orange', stroke_fill='black', stroke_width=2)
 
       Path(outpath).mkdir(parents=True, exist_ok=True)
       image.save(join(outpath, filename))
@@ -106,7 +117,7 @@ else:
 target_width = args.size[0]
 target_height = args.size[1]
 
-font = loadFont(['comic.ttf', 'arial.ttf'])
+font = loadFont(['comic.ttf', 'arial.ttf'], target_width, target_height)
 
 for filename in filenames:
   processImage(filepath, filename, args.outpath, target_width, target_height)
